b26a34055325c3b3257c0179807e28cd6e28736c
Nathn <nathan@tranchant.fr>
2025-02-28 18:31:18 +0000

    Add the limiter to every route

--- routes/index.js
+++ routes/index.js
@@ -19,44 +19,46 @@ const limiter = rateLimit({
 router.get('/', limiter, appController.indexPage);
 
 // Single tweet page
-router.get('/squak/:id', squakController.singleTweetPage);
-router.get('/squak/:id/json', APIController.SquakPage);
-router.post('/squak/:id', squakController.singleTweetPage);
-router.get('/reply/:id', squakController.singleReplyPage);
-router.post('/reply/:id', squakController.singleReplyPage);
+router.get('/squak/:id', limiter, squakController.singleTweetPage);
+router.get('/squak/:id/json', limiter, APIController.SquakPage);
+router.post('/squak/:id', limiter, squakController.singleTweetPage);
+router.get('/reply/:id', limiter, squakController.singleReplyPage);
+router.post('/reply/:id', limiter, squakController.singleReplyPage);
 
-router.post('/api/squaks/:id/heart', userController.heartTweet)
-router.post('/api/replies/:id/heart', userController.heartReply)
+router.post('/api/squaks/:id/heart', limiter, userController.heartTweet)
+router.post('/api/replies/:id/heart', limiter, userController.heartReply)
 router.post('/api/squaks/:id/reply',
+	limiter,
 	squakController.uploadImage,
 	squakController.postReply
 )
-router.get('/api/users/:id/verify', userController.verifyUser)
-router.get('/api/users/:id/confirm', userController.confirmUser)
-router.get('/api/users/:id/unconfirm', userController.unConfirmUser)
-router.get('/api/users/:id/follow', userController.followUser)
+router.get('/api/users/:id/verify', limiter, userController.verifyUser)
+router.get('/api/users/:id/confirm', limiter, userController.confirmUser)
+router.get('/api/users/:id/unconfirm', limiter, userController.unConfirmUser)
+router.get('/api/users/:id/follow', limiter, userController.followUser)
 
 // Registration page
-router.get('/register', userController.registerPage);
+router.get('/register', limiter, userController.registerPage);
 
 // Connexion page
-router.get('/login', userController.loginPage);
+router.get('/login', limiter, userController.loginPage);
 
 // Reset password page
 // router.get('/reset', userController.resetPage);
 
-router.get('/cgu', userController.cguPage);
+router.get('/cgu', limiter, userController.cguPage);
 
-router.get('/search', userController.searchPage);
+router.get('/search', limiter, userController.searchPage);
 
-router.get('/notifications', appController.notificationsPage);
+router.get('/notifications', limiter, appController.notificationsPage);
 
-router.get('/api', APIController.APIHomePage);
-router.get('/api/squaks', APIController.APIGetSquaks);
-router.get('/api/users', APIController.APIGetUsers);
+router.get('/api', limiter, APIController.APIHomePage);
+router.get('/api/squaks', limiter, APIController.APIGetSquaks);
+router.get('/api/users', limiter, APIController.APIGetUsers);
 
 // Registration POST request
 router.post('/register',
+	limiter,
 	userController.verifyRegister,
 	userController.checkUserExists,
 	userController.registerUser,
@@ -64,61 +66,68 @@ router.post('/register',
 )
 
 // Login POST action
-router.post('/login', authController.login);
+router.post('/login', limiter, authController.login);
 
 // Logout route
-router.get('/logout', authController.logout);
+router.get('/logout', limiter, authController.logout);
 
 
 // Account page
 router.get('/settings/account',
+	limiter,
 	authController.isLoggedIn,
 	userController.accountPage);
 router.get('/settings',
+	limiter,
 	authController.isLoggedIn,
 	userController.settingsPage);
 
 router.post('/settings/account',
+	limiter,
 	userController.upload,
 	userController.accountUpdate);
 
 router.post('/settings',
+	limiter,
 	userController.upload,
 	userController.settingsUpdate);
 
 // Uploading a profile image
 router.post('/upload',
+	limiter,
 	userController.upload,
 	userController.accountUpdate);
 
 // Tweet Specific routes
-///////////////////////////////
 router.post('/squak',
+	limiter,
 	squakController.uploadImage,
 	squakController.postTweet);
 
 router.get('/delete/:id',
+	limiter,
 	authController.isLoggedIn,
 	squakController.deleteSquak);
 
 router.get('/report/:id',
+	limiter,
 	authController.isLoggedIn,
 	squakController.reportSquak);
 
 router.post('/pin/:id',
+	limiter,
 	authController.isLoggedIn,
 	squakController.pinSquak);
 
 
-router.get('/:username/likes', userController.likesProfilePage);
-router.get('/:username/following', userController.followingProfilePage);
-router.get('/:username/followers', userController.followersProfilePage);
-router.get('/:username/json', APIController.ProfilePage);
+router.get('/:username/likes', limiter, userController.likesProfilePage);
+router.get('/:username/following', limiter, userController.followingProfilePage);
+router.get('/:username/followers', limiter, userController.followersProfilePage);
+router.get('/:username/json', limiter, APIController.ProfilePage);
 
 
 // Profile Page at the end because :username
-router.get('/:username', userController.profilePage);
-
+router.get('/:username', limiter, userController.profilePage);
 
 
 
